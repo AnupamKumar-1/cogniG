ceeee51e024d1677739390b83046d4e0d255548a
AnupamKumar-1 <anupam9594.kumar@gmail.com>
2025-07-14 23:47:11 +0530

    Minor Fixes

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -35,7 +35,7 @@ function Sidebar() {
         setCurrThreadId(newThreadId);
 
         try {
-            const response = await fetch(`http://localhost:8080/api/thread/${newThreadId}`);
+            const response = await fetch(`https://cognig-backend.onrender.com/api/thread/${newThreadId}`);
             const res = await response.json();
             console.log(res);
             setPrevChats(res);
@@ -48,7 +48,7 @@ function Sidebar() {
 
     const deleteThread = async (threadId) => {
         try {
-            const response = await fetch(`http://localhost:8080/api/thread/${threadId}`, {method: "DELETE"});
+            const response = await fetch(`https://cognig-backend.onrender.com/thread/${threadId}`, {method: "DELETE"});
             const res = await response.json();
             console.log(res);
 
