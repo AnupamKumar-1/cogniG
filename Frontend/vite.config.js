ad2e0b999aa5d3086bbf6bbd3e9ea75fd99e720e
AnupamKumar-1 <anupam9594.kumar@gmail.com>
2025-07-24 03:16:00 +0530

    Minor Fixes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,12 +5,17 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-
+      // Proxy /auth to backend
       '^/auth/.*': {
         target: 'https://cognig-backend.onrender.com',
         changeOrigin: true,
         secure: false,
-
+      },
+      // Proxy /api to backend
+      '^/api/.*': {
+        target: 'https://cognig-backend.onrender.com',
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
