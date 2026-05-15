1552ccdeecdb947ac97646261064d766d344c6b8
AnupamKumar-1 <anupam9594.kumar@gmail.com>
2026-05-15 14:44:48 +0530

    Minor fixes

--- Frontend/src/components/LoginModal.jsx
+++ Frontend/src/components/LoginModal.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./LoginModal.css";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
@@ -12,19 +11,20 @@ export default function LoginModal({ isOpen, onClose }) {
           <i className="fa-solid fa-xmark"></i>
         </button>
 
-        <div className="modal-logo">cogni<span>G</span></div>
+        <div className="modal-logo">
+          cogni<span>G</span>
+        </div>
         <div className="modal-sub">sign in to continue</div>
 
+        <a href={BACKEND_URL + "/auth/github"} className="btn-github">
+          <i className="fa-brands fa-github github-icon"></i>
+          Continue with GitHub
+        </a>
 
-        href={`${BACKEND_URL}/auth/github`}
-        className="btn-github"
-        >
-        <i className="fa-brands fa-github github-icon"></i>
-        Continue with GitHub
-      </a>
-
-      <p className="modal-terms">by continuing you agree to our terms of service</p>
+        <p className="modal-terms">
+          by continuing you agree to our terms of service
+        </p>
+      </div>
     </div>
-    </div >
   );
 }
